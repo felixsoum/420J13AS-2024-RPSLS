512b7af8bfc5224cdfa641fdc0c654628327062f
Felix Soumpholphakdy <felix@felixsoum.com>
2024-02-19 07:09:37 -0500

    Add base project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _420J13AS_2024_RPSLS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
